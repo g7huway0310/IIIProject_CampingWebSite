0e3333941edb33e2e09dd1b0072c096c05cfb6bf
EmmaLin123 <71500335+EmmaLin123@users.noreply.github.com>
2020-09-25 18:15:14 +0800

    third

--- EEIT124Project/src/shoppingMall/CookingDAO.java
+++ EEIT124Project/src/shoppingMall/CookingDAO.java
@@ -1,10 +1,11 @@
 package shoppingMall;
 
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class CookingDAO {
+public class CookingDAO{
 	
 	private Connection connection;
 	
@@ -12,11 +13,64 @@ public class CookingDAO {
 		this.connection = connection;
 	}
 	
-	
-	
-	
-	
-	
+public int createOrder(String cID, String cName) {
+	try {
+		int oID = 1;
+		String sqlString = "Select ORDERID.nextval From DUAL"; 
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery(sqlString);
+		
+		if(rs.next()) oID = rs.getInt(1);
+		
+		rs.close();
+		
+		sqlString = "INSERT INTO Order1(ORDERID,PRODUCTID) "+"VALUES("+ oID + ",'"+ cID + "')";
+		stmt.executeUpdate(sqlString);
+	  	stmt.close();
+	  	return oID;
+		
+	}catch(Exception e) {
+	    System.err.println("建立訂單時發生錯誤:" + e);
+		  return -1;
+	  }
+}
+
+public boolean deleteDept(int oID) {
+    try {
+      String sqlString = "DELETE FROM Order1 " +
+			                   "WHERE ORDERID = " + oID;
+	    Statement stmt = connection.createStatement();
+		  int deletecount = stmt.executeUpdate(sqlString);
+		  stmt.close();
+      if (deletecount >= 1) return true;
+		  else                  return false;
+	  } catch (Exception e) {
+	    System.err.println("刪除訂單時發生錯誤: "+ e);
+		  return false;
+	  }
+  }
+
+public boolean updateOrder(Cooking cooking,Order order ) {
+    try {
+      String sqlString = "UPDATE Order1" +
+	                  	   	 "SET PRODUCTID = '" + cooking.getcID() +"' "+
+	                  	     "PRODUCTNAME = '" + cooking.getcName() +"' "+
+	                  	     "PRICE = " + cooking.getcPrice() +
+	                  	     "BRAND = '" + cooking.getcBrand() +"' "+
+	                  	     "SPEC = '" + cooking.getcSpec() +"' "+
+	                  	     "Qty = " + order.getQty() +	                  	     
+                    		 "WHERE ORDERID = " + order.getOID();
+
+      Statement stmt = connection.createStatement();
+	    int updatecount = stmt.executeUpdate(sqlString);
+      stmt.close();
+      if (updatecount >= 1) return true;
+      else                  return false;
+	  } catch (Exception e) {
+	    System.err.println("更新訂單資料時發生錯誤:" + e);
+		  return false;
+    }
+  }
 	
 	public Cooking findCooking(String cName) {
 		try {
@@ -25,7 +79,7 @@ public class CookingDAO {
 			
 			Statement stmt = connection.createStatement();
 			String sqlString = "SELECT ID,PNAME,PRICE,BRAND,QTY "+
-					" From MALLCOOKING Where PNAME like"+ "'%"+ cName +"%'";
+					"From MALLCOOKING Where PNAME like"+ "'%"+ cName +"%'";
 			
 			System.out.println(sqlString);
 			
@@ -41,7 +95,7 @@ public class CookingDAO {
 			return cooking;
 			
 		}catch(Exception e) {
-			System.err.println("尋找部門資料時發生錯誤:" + e);
+			System.err.println("找不到此商品" + e);
 		    return null;
 		}
 		
